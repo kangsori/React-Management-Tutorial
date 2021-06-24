21a8e899fc8ccc0b53617532967396b78b3ea882
riversound <rkdthfl22@naver.com>
2021-06-24 21:57:22 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import logo from './logo.svg';
+import React , { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '강소리',
+  'birthday' : '940218',
+  'gender' : '여자',
+  'job' : '섹시한커리어우먼'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '노민성',
+  'birthday' : '960305',
+  'gender' : '여자',
+  'job' : '섹시한커리어우먼왼쪽닝겐'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '감동훈',
+  'birthday' : '960305',
+  'gender' : '남자',
+  'job' : '섹시한커리어우먼오른쪽닝겐'
+}
+]
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        { customers.map(c =>{
+             return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+             )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
